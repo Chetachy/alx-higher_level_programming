f9d218166654e41cfd32fb57b423a1a289a8c8be
Chetachy <chetyazuwike@gmail.com>
2022-11-30 11:12:06 -0800

    Task done

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: A singly-Linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+
+
+int check_cycle(listint_t *list)
+{
+    listint_t *ptr, *ptr1;
+
+    if (!list)
+        return (0);
+
+    ptr = list;
+    ptr1 = list->next;
+
+    while (ptr1)
+    {
+	    if (ptr1 == ptr)
+		    return (1);
+	    ptr1 = ptr1->next;  
+    }
+    return (0);
+}
---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,1 @@
+To learn more about Print "Hello World"
